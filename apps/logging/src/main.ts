bb3915beab57925a40d3f0f2ffc3cb16bc902a03
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-06-09 11:37:35 -0300

    feat: :sparkles: initializing rider-coordinates module

--- apps/logging/src/main.ts
+++ apps/logging/src/main.ts
@@ -3,6 +3,6 @@ import { LoggingModule } from './logging.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LoggingModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3002);
 }
 bootstrap();
