bb347970dc8dbd277bc924a4f2124cafad7e6c2e
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-06-10 11:20:38 -0300

    feat: :sparkles: setting up the rider coordinates schema

---
+++ apps/logging/src/rider-coordinates/schemas/rider-coordinates.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RiderCoordinateDocument = HydratedDocument<RiderCoordinate>;
+
+@Schema()
+class RiderCoordinate {
+  @Prop({ required: true })
+  lat: number;
+
+  @Prop({ required: true })
+  lng: number;
+
+  @Prop({ required: true })
+  rider: string;
+}
+
+export const RiderCoordinateSchema =
+  SchemaFactory.createForClass(RiderCoordinate);
